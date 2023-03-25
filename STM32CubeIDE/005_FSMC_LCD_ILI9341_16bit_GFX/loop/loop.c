005c1c0a8cc10358116439f6ead6b8ec676a8370
DamianPodrazaCode <damian.podraza@gmail.com>
2023-03-25 08:27:06 +0100

    Signed-off-by: DamianPodrazaCode <damian.podraza@gmail.com>

--- STM32CubeIDE/005_FSMC_LCD_ILI9341_16bit_GFX/loop/loop.c
+++ STM32CubeIDE/005_FSMC_LCD_ILI9341_16bit_GFX/loop/loop.c
@@ -34,20 +34,15 @@ void setup() {
 	time = HAL_GetTick() - time;
 }
 
-typedef struct {
-	int32_t x;
-	int32_t y;
-} point2d_t;
-
 #define PI 3.14159265
-void rotateStep(point2d_t *pIn, point2d_t *pOut, float deg) {
+void rotateStep(gfx2dPoint_t *pIn, gfx2dPoint_t *pOut, float deg) {
 	pOut->x = ((pIn->x - 160) * cos(deg * PI / 180.0) - (pIn->y - 120) * sin(deg * PI / 180.0)) + 160;
 	pOut->y = ((pIn->x - 160) * sin(deg * PI / 180.0) + (pIn->y - 120) * cos(deg * PI / 180.0)) + 120;
 	deg++;
 }
 
-point2d_t A = { 160, 50 }, B = { 210, 190 }, C = { 110, 190 };
-point2d_t Ap, Bp, Cp;
+gfx2dPoint_t A = { 160, 50 }, B = { 210, 190 }, C = { 110, 190 };
+gfx2dPoint_t Ap, Bp, Cp;
 float deg = 0;
 
 void bench4() {
@@ -55,25 +50,27 @@ void bench4() {
 	rotateStep(&A, &Ap, deg);
 	rotateStep(&B, &Bp, deg);
 	rotateStep(&C, &Cp, deg);
-	gfx2d_fillTriangle(Ap.x, Ap.y, Bp.x, Bp.y, Cp.x, Cp.y, COLOR_BLUE);
+	gfx2d_fillTriangle(Ap, Bp, Cp, COLOR_BLUE);
 	HAL_Delay(9);
-	gfx2d_fillTriangle(Ap.x, Ap.y, Bp.x, Bp.y, Cp.x, Cp.y, COLOR_BLACK);
+	gfx2d_fillTriangle(Ap, Bp, Cp, COLOR_BLACK);
 	deg = deg + 1;
 }
 
 void bench3() {
 	srand(HAL_GetTick());
-	uint32_t x0, x1, x2, y0, y1, y2, color;
+	uint32_t color;
+	gfx2dPoint_t A, B, C;
 	const uint32_t iter = 2000;
 	for (int i = 0; i < iter; i++) {
-		x0 = rand() % lcdProp.width;
-		x1 = rand() % lcdProp.width;
-		x2 = rand() % lcdProp.width;
-		y0 = rand() % lcdProp.height;
-		y1 = rand() % lcdProp.height;
-		y2 = rand() % lcdProp.height;
+		A.x = rand() % lcdProp.width;
+		B.x = rand() % lcdProp.width;
+		C.x = rand() % lcdProp.width;
+		A.y = rand() % lcdProp.height;
+		B.y = rand() % lcdProp.height;
+		C.y = rand() % lcdProp.height;
 		color = rand() % 0xffff;
-		gfx2d_fillTriangle(x0, y0, x1, y1, x2, y2, color);
+		gfx2d_fillTriangle(A, B, C, color);
+		//gfx2d_triangle(A, B, C, color);
 	}
 }
 
@@ -84,22 +81,22 @@ void bench2() {
 //	for (int x = 0; x < lcdProp.width/2; x++)
 //		gfx2d_line(x, 0, lcdProp.width / 2, lcdProp.height / 2, COLOR_CYAN);
 
-	for (int x = 0; x < lcdProp.width; x++) {
-		gfx2d_line(x, 0, lcdProp.width / 2, lcdProp.height / 2, COLOR_CYAN);
-//		HAL_Delay(9);
-	}
-	for (int y = 0; y < lcdProp.height; y++) {
-		gfx2d_line(lcdProp.width, y, lcdProp.width / 2, lcdProp.height / 2, COLOR_BLUE);
-//		HAL_Delay(9);
-	}
-	for (int x = lcdProp.width; x >= 0; x--) {
-		gfx2d_line(x, lcdProp.height, lcdProp.width / 2, lcdProp.height / 2, COLOR_GREEN);
-//		HAL_Delay(9);
-	}
-	for (int y = lcdProp.height; y >= 0; y--) {
-		gfx2d_line(0, y, lcdProp.width / 2, lcdProp.height / 2, COLOR_MAGENTA);
-//		HAL_Delay(9);
-	}
+//	for (int x = 0; x < lcdProp.width; x++) {
+//		gfx2d_line(x, 0, lcdProp.width / 2, lcdProp.height / 2, COLOR_CYAN);
+////		HAL_Delay(9);
+//	}
+//	for (int y = 0; y < lcdProp.height; y++) {
+//		gfx2d_line(lcdProp.width, y, lcdProp.width / 2, lcdProp.height / 2, COLOR_BLUE);
+////		HAL_Delay(9);
+//	}
+//	for (int x = lcdProp.width; x >= 0; x--) {
+//		gfx2d_line(x, lcdProp.height, lcdProp.width / 2, lcdProp.height / 2, COLOR_GREEN);
+////		HAL_Delay(9);
+//	}
+//	for (int y = lcdProp.height; y >= 0; y--) {
+//		gfx2d_line(0, y, lcdProp.width / 2, lcdProp.height / 2, COLOR_MAGENTA);
+////		HAL_Delay(9);
+//	}
 
 }
 
@@ -112,36 +109,36 @@ void loop() {
 }
 
 void bench() {
-	srand(HAL_GetTick());
-	uint32_t x1, x2, y1, y2, color;
-	const uint32_t iter = 2000;
-	for (int i = 0; i < iter; i++) {
-		x1 = rand() % lcdProp.width;
-		x2 = rand() % lcdProp.width;
-		y1 = rand() % lcdProp.height;
-		color = rand() % 0xffff;
-		drawHLine(x1, x2, y1, color);
-	}
-	for (int i = 0; i < iter; i++) {
-		x1 = rand() % lcdProp.width;
-		y1 = rand() % lcdProp.height;
-		y2 = rand() % lcdProp.height;
-		color = rand() % 0xffff;
-		drawVLine(x1, y1, y2, color);
-	}
-	for (int i = 0; i < iter; i++) {
-		x1 = rand() % lcdProp.width;
-		x2 = rand() % lcdProp.width;
-		y1 = rand() % lcdProp.height;
-		y2 = rand() % lcdProp.height;
-		color = rand() % 0xffff;
-		gfx2d_line(x1, y1, x2, y2, color);
-	}
-	for (int i = 0; i < iter * 100; i++) {
-		x1 = rand() % lcdProp.width;
-		y1 = rand() % lcdProp.height;
-		color = rand() % 0xffff;
-		drawPixel(x1, y1, color);
-	}
-	clearScr(COLOR_BLACK);
+//	srand(HAL_GetTick());
+//	uint32_t x1, x2, y1, y2, color;
+//	const uint32_t iter = 2000;
+//	for (int i = 0; i < iter; i++) {
+//		x1 = rand() % lcdProp.width;
+//		x2 = rand() % lcdProp.width;
+//		y1 = rand() % lcdProp.height;
+//		color = rand() % 0xffff;
+//		drawHLine(x1, x2, y1, color);
+//	}
+//	for (int i = 0; i < iter; i++) {
+//		x1 = rand() % lcdProp.width;
+//		y1 = rand() % lcdProp.height;
+//		y2 = rand() % lcdProp.height;
+//		color = rand() % 0xffff;
+//		drawVLine(x1, y1, y2, color);
+//	}
+//	for (int i = 0; i < iter; i++) {
+//		x1 = rand() % lcdProp.width;
+//		x2 = rand() % lcdProp.width;
+//		y1 = rand() % lcdProp.height;
+//		y2 = rand() % lcdProp.height;
+//		color = rand() % 0xffff;
+//		gfx2d_line(x1, y1, x2, y2, color);
+//	}
+//	for (int i = 0; i < iter * 100; i++) {
+//		x1 = rand() % lcdProp.width;
+//		y1 = rand() % lcdProp.height;
+//		color = rand() % 0xffff;
+//		drawPixel(x1, y1, color);
+//	}
+//	clearScr(COLOR_BLACK);
 }
